639b5f6e87425c16de3215f3f7cb9aa9de30dbbd
zoomll9833 <caidenleggett@gmail.com>
2024-10-20 14:55:36 -0400

    IM RIPPING MY HAIR OUT IN AN ATTEMPT TO GET THIS TO RUN 😭

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,21 @@
 package org.zoomll.scalculator
 
+import com.sun.org.apache.xalan.internal.lib.ExsltStrings.padding
 import scalafx.application.JFXApp3
+import scalafx.geometry.Insets
 import scalafx.scene.Scene
+import scalafx.scene.layout.StackPane
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
 
 abstract class Main extends JFXApp3:
-  override def start():Unit = {
-    stage = new JFXApp3.PrimaryStage {
+  override def start():Unit =
+    stage = new JFXApp3.PrimaryStage:
       title = "Test"
-      scene = new Scene
-    }
-  }
+      scene = new Scene:
+        root = new StackPane:
+          padding = Insets(30)
+          content = new Rectangle:
+            width = 200
+            height = 200
+            fill = Color.Gold
