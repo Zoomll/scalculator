af7c82861a2099c9fc7511034c3f54292af997b0
zoomll9833 <caidenleggett@gmail.com>
2024-10-20 13:52:49 -0400

    Test window

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+package org.zoomll.scalculator
+
+import scalafx.application.JFXApp3
+import scalafx.scene.Scene
+
+abstract class Main extends JFXApp3
+  val stage = JFXApp3.PrimaryStage()
+  val scene = Scene(200,200)
--- src/main/scala/main.scala
+++
@@ -1,1 +0,0 @@
-package org.zoomll.scalculator
