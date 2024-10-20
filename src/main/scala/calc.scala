8b5ac377402b425a6e5384e7cd9c1ce319364c26
zoomll9833 <caidenleggett@gmail.com>
2024-10-20 12:55:10 -0400

    Added mult and divi methods

--- src/main/scala/calc.scala
+++ src/main/scala/calc.scala
@@ -2,5 +2,20 @@ package  org.zoomll.scalculator
 
 object  calc:
   def add(a:Double,b:Double): Double = a + b
-  
+
   def sub(a:Double,b:Double): Double = a - b
+
+  def mult(a:Double,b:Double): Double = a * b
+
+  def divi(a:Double,b:Double): Double =
+    if b != 0.toDouble then
+      val ans = a / b
+    else println("Error: Divide by 0 is not possible")
+    
+    ans
+    
+  
+
+    
+  
+  
