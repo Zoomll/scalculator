b07ba122a54b2e7910ba7553223a2eb9b2fe3ec9
zoomll9833 <caidenleggett@gmail.com>
2024-11-01 18:23:06 -0400

    tysm for getting it working @makingthematrix!

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,17 +5,19 @@ import scalafx.application.JFXApp3
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.layout.StackPane
-import scalafx.scene.paint.Color
+import scalafx.scene.paint.Color._
 import scalafx.scene.shape.Rectangle
 
-abstract class Main extends JFXApp3:
+object Main extends JFXApp3:
   override def start():Unit =
     stage = new JFXApp3.PrimaryStage:
       title = "Test"
+      width = 500
+      height = 500
       scene = new Scene:
-        root = new StackPane:
-          padding = Insets(30)
-          content = new Rectangle:
-            width = 200
-            height = 200
-            fill = Color.Gold
+        fill = LightGreen
+        content = new Rectangle:
+          x = 25
+          y = 40
+          width = 100
+          height = 100
