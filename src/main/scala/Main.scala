f482bc0164ac6ae7becfdceac83efff61c3ca645
zoomll9833 <caidenleggett@gmail.com>
2024-11-01 19:50:10 -0400

    added some currently nonfunctional ui

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,28 @@
 package org.zoomll.scalculator
 
-import com.sun.org.apache.xalan.internal.lib.ExsltStrings.padding
+import scalafx.Includes.*
 import scalafx.application.JFXApp3
-import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.layout.StackPane
-import scalafx.scene.paint.Color._
-import scalafx.scene.shape.Rectangle
+import scalafx.scene.control.{Button, TextField}
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color.*
 
 object Main extends JFXApp3:
   override def start():Unit =
     stage = new JFXApp3.PrimaryStage:
-      title = "Test"
+      title = "Scalculator"
       width = 500
-      height = 500
+      height = 600
+      resizable = false
       scene = new Scene:
-        fill = LightGreen
-        content = new Rectangle:
-          x = 25
-          y = 40
-          width = 100
-          height = 100
+        fill = Red
+        content = new HBox {
+          children = Seq(
+            new TextField {
+              style = "-fx-font: normal 44pt monospace"
+            },
+            new Button {
+
+            }
+          )
+        }
